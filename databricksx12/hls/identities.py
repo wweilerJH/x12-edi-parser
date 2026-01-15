00da76281da4f0c47f6222dbd89abd7d3be83311
Will Weiler <wweiler1@jh.edu>
2026-01-15 12:31:45 -0500

    adds condition codes too Identities.py ClaimIdentity Class

--- databricksx12/hls/identities.py
+++ databricksx12/hls/identities.py
@@ -74,6 +74,12 @@ class ClaimIdentity(Identity):
                 [{'prcdr_cd': s.element(i,1), 'date_format': s.element(i,2), 'date': s.element(i,3)} for i in list(range(1, s.segment_len()))]
             for s in other_hi])) 
         }
+        self.condition_codes = [  
+            s.element(i, 2)  
+            for s in hi if s.element(1, 1) == 'BG'  
+            for i in range(1, s.segment_len())  
+            if s.element(i, 2)  
+            ]  
 
 # POA is the last sub element of the respective segments
 class DiagnosisIdentity(Identity):
