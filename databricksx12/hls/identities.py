cf68746050e534149cdcd273b272ea9e9ca0af09
Will Weiler <wweiler1@jh.edu>
2026-01-15 12:45:57 -0500

    fixes main

--- databricksx12/hls/identities.py
+++ databricksx12/hls/identities.py
@@ -74,13 +74,6 @@ class ClaimIdentity(Identity):
                 [{'prcdr_cd': s.element(i,1), 'date_format': s.element(i,2), 'date': s.element(i,3)} for i in list(range(1, s.segment_len()))]
             for s in other_hi])) 
         }
-        his = hi if isinstance(hi, list) else [hi]  
-        self.condition_codes = list(itertools.chain.from_iterable(  
-            [  
-            [s.element(i, 2) for i in range(1, s.segment_len()) if s.element(i, 1) == 'BG' and s.element(i, 2)]  
-            for s in his  
-            ]  
-                )) 
 
 # POA is the last sub element of the respective segments
 class DiagnosisIdentity(Identity):
